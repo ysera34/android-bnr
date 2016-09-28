beb4f3b2a363e7b20cee89ff3e549d0be5189fe1
ysera34 <ycyoon34@gmail.com>
2016-09-28 17:08:02 +0900

    chapter 3

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,12 +2,16 @@ package com.bignerdranch.android.geoquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private static final String TAG = QuizActivity.class.getSimpleName();
+    private static final String KEY_INDEX = "index";
 
     private TextView mQuestionTextView;
     private Button mTrueButton;
@@ -37,6 +41,12 @@ public class QuizActivity extends AppCompatActivity {
 
 //        int question = mQuestionsBank[mCurrentIndex].getTextResId();
 //        mQuestionTextView.setText(question);
+
+        // get Bundle Date savedInstanceState
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
         updateQuestion();
 
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
@@ -79,6 +89,13 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+        outState.putInt(KEY_INDEX, mCurrentIndex);
     }
 
     private void updateQuestion() {
