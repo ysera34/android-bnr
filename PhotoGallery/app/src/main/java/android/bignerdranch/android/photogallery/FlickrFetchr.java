47cac0e1615c66b45eabd3cebe7e62f2df1ed608
ysera34 <ycyoon34@gmail.com>
2016-11-26 14:00:25 +0900

    add gallery model

--- PhotoGallery/app/src/main/java/android/bignerdranch/android/photogallery/FlickrFetchr.java
+++ PhotoGallery/app/src/main/java/android/bignerdranch/android/photogallery/FlickrFetchr.java
@@ -1,5 +1,8 @@
 package android.bignerdranch.android.photogallery;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,6 +14,9 @@ import java.net.URL;
  */
 
 public class FlickrFetchr {
+
+    private static final String TAG = FlickrFetchr.class.getSimpleName();
+//    private static final String API_KEY = R.string.flickr_api_key;
 
     public byte[] getUrlBtyes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
@@ -39,5 +45,23 @@ public class FlickrFetchr {
 
     public String getUrlString(String urlSpec) throws IOException {
         return new String(getUrlBtyes(urlSpec));
+    }
+
+    public void fetchItems(String key) {
+        try {
+            String url = Uri.parse("https://api.flickr.com/services/rest/")
+                    .buildUpon()
+                    .appendQueryParameter("method", "flickr.photos.getRecent")
+                    .appendQueryParameter("api_key", key)
+                    .appendQueryParameter("format", "json")
+                    .appendQueryParameter("nojsoncallback", "1")
+                    .appendQueryParameter("extras", "url_s")
+                    .build().toString();
+            String jsonString = getUrlString(url);
+            Log.i(TAG, "Received JSON : " + jsonString);
+        } catch (IOException ioe) {
+            Log.e(TAG, "Failed to fetch items ", ioe);
+        }
+
     }
 }
--- PhotoGallery/app/src/main/java/android/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/android/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -6,12 +6,9 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.io.IOException;
 
 /**
  * Created by yoon on 2016. 11. 26..
@@ -53,19 +50,23 @@ public class PhotoGalleryFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+//        Log.d(TAG, getString(R.string.flickr_api_key));
+//        Log.d(TAG, getString(R.string.flickr_api_password));
     }
 
     private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
 
         @Override
         protected Void doInBackground(Void... voids) {
-            try {
-                String result = new FlickrFetchr()
-                        .getUrlString("http://develop.inframincer.org");
-                Log.i(TAG, "Fetched contents of URL : " + result);
-            } catch (IOException ioe) {
-                Log.e(TAG, "Failed to fetch URL: ", ioe);
-            }
+//            try {
+//                String result = new FlickrFetchr()
+//                        .getUrlString("http://develop.inframincer.org");
+//                Log.i(TAG, "Fetched contents of URL : " + result);
+//            } catch (IOException ioe) {
+//                Log.e(TAG, "Failed to fetch URL: ", ioe);
+//            }
+            new FlickrFetchr().fetchItems(getString(R.string.flickr_api_key));
             return null;
         }
 
